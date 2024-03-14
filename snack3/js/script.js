6b6fdb643e08bfca4ec0b7d86a8839cc2c290bc5
itsjackie00 <jackiedanso33@gmail.com>
2024-03-14 12:14:10 +0100

    added getrndinteger function

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,26 +1,18 @@
 //alert('funziona?');
 
 
-// SNACK 2
-
-// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
-// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
-
-// const numeri = [];
-// let somma = 0;
-
-// while (somma < 50) {
-//     const newNumero = parseInt(prompt("Inserisci un nuovo numero"));
-//     if (!isNaN(newNumero)) {
-//         somma += newNumero;
-//         numeri.push(newNumero);
-//     }
-// }
-// console.log("Numeri inseriti:", numeri);
-// console.log("Somma:", somma);
-
-
 // SNACK 3
+/*
+    Fai inserire un numero, che chiameremo N, all'utente.
+    Genera con una funzione N array, ognuno formato da 10 numeri casuali da 1 a 100.
+    Ogni volta che ne crei uno, stampalo.
+ */
 
+const numbers = parseInt(prompt('Scegli un numero'));
+console.log(numbers);
 
-
+function getRndInteger() {
+    let array = [];
+    for (let i = 0; i < 10; i++) {
+    }
+}
